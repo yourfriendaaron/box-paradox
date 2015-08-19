374d75aef1f1b04cdf4a3f81fd12e8fe184826dc
yourfriendaaron <yourfriendaaron@users.noreply.github.com>
2015-08-18 23:48:19 -0400

    updated game to v0.2

--- box paradox game.py
+++ box paradox game.py
@@ -6,6 +6,9 @@ numright = 0
 numwrong = 0
 g = re.compile('G', re.IGNORECASE)
 s = re.compile('S', re.IGNORECASE)
+yes = re.compile('YES', re.IGNORECASE)
+cheat = False
+quitgame = False
 
 def get_guess():
     guess = str(input('Type G to guess that the other coin will be gold. Type S to guess silver.\n'))
@@ -15,21 +18,21 @@ def get_guess():
     return guess
 
 def samebox(boxtype, rightcoin):
-    print('you have drawn a', boxtype, 'coin.')
+    print('You reach into the box without looking, and draw a', boxtype, 'coin.')
     guess = get_guess()
     input('press Enter to look at the other coin in this box...')
     if guess == rightcoin:
-        print('you were right!')
+        print('You were right!')
         global numright
         numright += 1
     else:
-        print('you were wrong :(')
+        print('You were wrong :(')
         global numwrong
         numwrong += 1
 
 def diffbox():
     coindraw = random.choice(['gold', 'silver'])
-    print('you have drawn a', coindraw, 'coin.')
+    print('You reach into the box without looking, and draw a', coindraw, 'coin.')
     if coindraw == 'gold':
         rightcoin = 'S'
     if coindraw == 'silver':
@@ -37,24 +40,38 @@ def diffbox():
     guess = get_guess()
     input('press Enter to look at the other coin in this box...')
     if guess == rightcoin:
-        print('you were right!')
+        print('You were right!')
         global numright
         numright += 1
     else:
-        print('you were wrong :(')
+        print('You were wrong :(')
         global numwrong
         numwrong += 1
 
-while playagain == 'y':
+while True:
+    startgame = str(input('There are three boxes in front of you.\nOne has two silver coins, another has a silver and a gold coin, and the remaining box contains two gold coins.\nWould you like to play a game? Type YES to continue...\n'))
+    if yes.match(startgame):
+        break
+    if startgame == 'UUDDLRLRBA':
+        cheat = True
+        break
+
+while quitgame != 'Q':
+    print('''     _______      _______      _______
+    |       |    |       |    |       |
+    | box 1 |    | box 2 |    | box 3 |
+    |_______|    |_______|    |_______|''')
+    input('Select a box. Enter 1 2 or 3...\n')
     box = random.choice(['box SS', 'box SG', 'box GG'])
-    print(box)
+    if cheat == True:
+        print(box)
     if box == 'box SS':
         samebox('silver', 'S')
     if box == 'box GG':
         samebox('gold', 'G')
     if box == 'box SG':
         diffbox()
-    playagain = input('Play again? y or n\n')
+    quitgame = input('Press Enter to play again or type Q to quit and see your results...')
 
 accuracy = (numright / (numright + numwrong)) * 100
 
